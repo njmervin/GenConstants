017ba051903a85e710eeda89e96c655f71927efc
njmervin <glazesoft@gmail.com>
2019-01-12 16:33:04 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# GenConstants
-Generate constant definitions for various programming languages through configuration files.
+﻿# GenConstants
+Generate constant definitions for various programming languages through configuration file.
+
+## usage
+```
+usage: java -jar GenConstants [option]
+ -h,--help         print help
+    --in <arg>     configuration file path
+    --lang <arg>   program language
+    --out <arg>    constant definition file path
+```
+
+## configuration file syntax
+
+```
+$package=zen.puzzle
+$name=Constants
+
+folder_type.normal          =0 #normal folder
+folder_type.normal_root     =1 #normal folder root
+folder_type.recycle         =2 #recycle folder
+folder_type.placeholder     =99 #place holder folder
+```
+**$package** \
+used for java language
+
+**$name** \
+generated class name
+
+**constant define format** \
+\<group>.\<key>=\<value> #\<comment>
+
+## output sample
+### Java
+```
+java -jar GenConstants-1.0.0.jar --lang Java --in "D:\Code\github\puzzle\docs\constants.properties" --out "D:\Code\github\puzzle\puzzleserver\src\main\java"
+```
+File: Constants.java
+```Java
+package zen.puzzle;
+
+public class Constants {
+    public static final int FOLDER_TYPE_NORMAL      = 0;  //normal folder
+    public static final int FOLDER_TYPE_NORMAL_ROOT = 1;  //normal folder root
+    public static final int FOLDER_TYPE_RECYCLE     = 2;  //recycle folder
+    public static final int FOLDER_TYPE_PLACEHOLDER = 99; //place holder folder
+
+}
+```
+
+### C++
+```
+java -jar GenConstants-1.0.0.jar --lang C++ --in "D:\Code\github\puzzle\docs\constants.properties" --out "D:\Code\github\puzzle\puzzle"
+```
+File: Constants.h
+```C++
+#pragma once
+
+class Constants
+{
+public:
+    static const int FOLDER_TYPE_NORMAL      = 0;  //normal folder
+    static const int FOLDER_TYPE_NORMAL_ROOT = 1;  //normal folder root
+    static const int FOLDER_TYPE_RECYCLE     = 2;  //recycle folder
+    static const int FOLDER_TYPE_PLACEHOLDER = 99; //place holder folder
+
+};
+```
---
+++ src/main/java/GenConstants.java
@@ -0,0 +1,230 @@
+import org.apache.commons.cli.*;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class ConstantInfo {
+    public String group;
+    public String key;
+    public String value;
+    public String comment;
+
+    public String getValueType(String lang) {
+        if(value.startsWith("\"")) {
+            if (lang.equalsIgnoreCase("Java"))
+                return "String";
+            else if(lang.equalsIgnoreCase("C++"))
+                return "const char *";
+            else
+                return null;
+        }
+        else
+            return "int";
+    }
+
+    public String getIdentifier() {
+        return (group + "_" + key).toUpperCase();
+    }
+}
+
+public class GenConstants {
+    private static String output_package;
+    private static String output_name;
+    private static Map<String, List<ConstantInfo>>  output_constants = new HashMap<String, List<ConstantInfo>>();
+
+    public static void main(String[] args) throws ParseException, IOException {
+        Options options = new Options();
+        options.addOption("h", "help", false, "print help");
+        options.addOption(null, "lang", true, "program language");
+        options.addOption(null, "in", true, "configuration file path");
+        options.addOption(null, "out", true, "contain constant definition file directory");
+        CommandLine cmd = new DefaultParser().parse(options, args);
+
+        if(cmd.getOptions().length == 0 || cmd.hasOption('h')) {
+            new HelpFormatter().printHelp("java -jar GenConstants [option]", options);
+            return;
+        }
+
+        if(!cmd.hasOption("lang") || !cmd.hasOption("in") || !cmd.hasOption("out")) {
+            new HelpFormatter().printHelp("java -jar GenConstants [option]", options);
+            return;
+        }
+
+        parseInput(cmd.getOptionValue("in"));
+
+        if(output_package == null) {
+            System.err.println("$package not found!");
+            return;
+        }
+
+        if(output_name == null) {
+            System.err.println("$name not found!");
+            return;
+        }
+
+        String lang = cmd.getOptionValue("lang");
+        if(lang.equalsIgnoreCase("Java"))
+            outputJava(cmd.getOptionValue("out"));
+        else if(lang.equalsIgnoreCase("C++"))
+            outputCpp(cmd.getOptionValue("out"));
+        else {
+            System.err.println("language " + lang + " is not supported.");
+            return;
+        }
+    }
+
+    private static void parseInput(String inputfile) throws IOException {
+        String line;
+        BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(inputfile),"UTF-8"));
+
+        while((line = in.readLine()) != null) {
+            line = line.trim();
+            if(line.isEmpty()) {
+                continue;
+            }
+
+            int pos;
+            String group = "", key = "", value = "", comment = "";
+
+            pos = line.indexOf('#');
+            if(pos != -1) {
+                comment = line.substring(pos + 1).trim();
+                line = line.substring(0, pos);
+            }
+
+            pos = line.indexOf('=');
+            if(pos == -1)
+                continue;
+            value = line.substring(pos + 1).trim();
+            line = line.substring(0, pos).trim();
+            if(line.equals("$package")) {
+                output_package = value;
+                continue;
+            }
+            else if(line.equals("$name")) {
+                output_name = value;
+                continue;
+            }
+
+            pos = line.indexOf('.');
+            if(pos == -1)
+                continue;
+            key = line.substring(pos + 1);
+            group = line.substring(0, pos);
+
+            ConstantInfo info = new ConstantInfo();
+            info.group = group;
+            info.key = key;
+            info.value = value;
+            info.comment = comment;
+
+            List<ConstantInfo> list = output_constants.get(group.toLowerCase());
+            if(list == null) {
+                list = new ArrayList<ConstantInfo>();
+                output_constants.put(group.toLowerCase(), list);
+            }
+            list.add(info);
+        }
+
+        in.close();
+    }
+
+    private static int[] getKeyValueLength(List<ConstantInfo> list) {
+        int[] lens = new int[]{0, 0};
+
+        for(ConstantInfo info : list) {
+            lens[0] = Math.max(lens[0], info.getIdentifier().length());
+            lens[1] = Math.max(lens[1], info.value.length());
+        }
+
+        return lens;
+    }
+
+    private static void outputJava(String outdir) throws IOException {
+        File file = new File(outdir + "\\" + output_package.replace('.', '\\') + "\\" + output_name + ".java");
+        PrintStream out = new PrintStream(file, "utf-8");
+
+        out.println("package " + output_package + ";");
+        out.println();
+        out.println("public class " + output_name + " {");
+
+        for(List<ConstantInfo> list : output_constants.values()) {
+            int[] lens = getKeyValueLength(list);
+
+            for(ConstantInfo info : list) {
+                out.print("    public static final ");
+                out.print(info.getValueType("Java"));
+                out.print(" ");
+                out.print(info.getIdentifier());
+
+                for(int i=0; i<lens[0] - info.getIdentifier().length(); i++)
+                    out.print(" ");
+
+                out.print(" = ");
+                out.print(info.value);
+                out.print(";");
+                if(!info.comment.isEmpty()) {
+                    for(int i=0; i<lens[1] - info.value.length(); i++)
+                        out.print(" ");
+
+                    out.print(" //");
+                    out.print(info.comment);
+                }
+                out.println();
+            }
+
+
+            out.println();
+        }
+
+        out.println("}");
+
+        out.close();
+    }
+
+    private static void outputCpp(String outdir) throws IOException {
+        File file = new File(outdir + "\\" + output_name.toLowerCase() + ".h");
+        PrintStream out = new PrintStream(file, "utf-8");
+
+        out.println("#pragma once");
+        out.println();
+        out.println("class " + output_name);
+        out.println("{");
+        out.println("public:");
+
+        for(List<ConstantInfo> list : output_constants.values()) {
+            int[] lens = getKeyValueLength(list);
+
+            for(ConstantInfo info : list) {
+                out.print("    static const ");
+                out.print(info.getValueType("C++"));
+                out.print(" ");
+                out.print(info.getIdentifier());
+
+                for(int i=0; i<lens[0] - info.getIdentifier().length(); i++)
+                    out.print(" ");
+
+                out.print(" = ");
+                out.print(info.value);
+                out.print(";");
+                if(!info.comment.isEmpty()) {
+                    for(int i=0; i<lens[1] - info.value.length(); i++)
+                        out.print(" ");
+
+                    out.print(" //");
+                    out.print(info.comment);
+                }
+                out.println();
+            }
+
+            out.println();
+        }
+
+        out.println("};");
+
+        out.close();
+    }
+}
