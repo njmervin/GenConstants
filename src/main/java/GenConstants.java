e2580a288398917a308d29971090d9c1152d3a9c
njmervin <glazesoft@gmail.com>
2019-01-19 23:16:11 +0800

    fix: chinese string support

--- src/main/java/GenConstants.java
+++ src/main/java/GenConstants.java
@@ -14,7 +14,7 @@ class ConstantInfo {
             if (lang.equalsIgnoreCase("Java"))
                 return "String";
             else if(lang.equalsIgnoreCase("C++"))
-                return "const char *";
+                return "constexpr char*";
             else
                 return null;
         }
@@ -24,6 +24,58 @@ class ConstantInfo {
 
     public String getIdentifier() {
         return (group + "_" + key).replace('.', '_').toUpperCase();
+    }
+
+    public String getPrefix(String lang) {
+        if(lang.equalsIgnoreCase("Java"))
+            return "public static final";
+        else if(lang.equalsIgnoreCase("C++"))
+            return "static const";
+        else
+            return null;
+    }
+
+    public String getValueCode(String lang) {
+        if(value.startsWith("\"")) {
+            if (lang.equalsIgnoreCase("Java"))
+                return value;
+            else if(lang.equalsIgnoreCase("C++")) {
+                try {
+                    byte[] bytes = value.substring(1, value.length() - 1).getBytes("utf-8");
+                    StringBuilder sb = new StringBuilder(bytes.length * 3);
+                    sb.append('"');
+                    for(int b : bytes) {
+                        sb.append("\\x");
+                        b = (b + 256 ) % 256;
+                        if(b < 0x10) {
+                            sb.append('0');
+                            sb.append(Integer.toHexString(b));
+                        }
+                        else
+                            sb.append(Integer.toHexString(b));
+                    }
+                    sb.append('"');
+                    return sb.toString();
+                } catch (UnsupportedEncodingException e) {
+                    return null;
+                }
+            }
+            else
+                return null;
+        }
+
+        return value;
+    }
+
+    public String getComment(String lang) {
+        if(value.startsWith("\"")) {
+            if(lang.equalsIgnoreCase("C++")) {
+                if (comment == null || comment.isEmpty())
+                    return value;
+            }
+        }
+
+        return comment;
     }
 }
 
@@ -72,9 +124,9 @@ public class GenConstants {
 
         String lang = cmd.getOptionValue("lang");
         if(lang.equalsIgnoreCase("Java"))
-            outputJava(cmd.getOptionValue("out"));
+            outputJava(lang, cmd.getOptionValue("out"));
         else if(lang.equalsIgnoreCase("C++"))
-            outputCpp(cmd.getOptionValue("out"));
+            outputCpp(lang, cmd.getOptionValue("out"));
         else {
             System.err.println("language " + lang + " is not supported.");
             return;
@@ -137,18 +189,58 @@ public class GenConstants {
         in.close();
     }
 
-    private static int[] getKeyValueLength(List<ConstantInfo> list) {
-        int[] lens = new int[]{0, 0};
+    private static int[] getTypeKeyValueLength(String lang, List<ConstantInfo> list) {
+        int[] lens = new int[]{0, 0, 0};
 
         for(ConstantInfo info : list) {
-            lens[0] = Math.max(lens[0], info.getIdentifier().length());
-            lens[1] = Math.max(lens[1], info.value.length());
+            lens[0] = Math.max(lens[0], info.getPrefix(lang).length() + 1 + info.getValueType(lang).length());
+            lens[1] = Math.max(lens[1], info.getIdentifier().length());
+            lens[2] = Math.max(lens[2], info.value.length());
         }
 
         return lens;
     }
 
-    private static void outputJava(String outdir) throws IOException {
+    private static void outputItems(String lang, PrintStream out) {
+        String text;
+
+        for(String key : group_order_list) {
+            List<ConstantInfo> list = output_constants.get(key);
+            int[] lens = getTypeKeyValueLength(lang, list);
+
+            for(ConstantInfo info : list) {
+                out.print("    ");
+                out.print(info.getPrefix(lang));
+                out.print(" ");
+                out.print(info.getValueType(lang));
+                for(int i=0; i<lens[0] - info.getPrefix(lang).length() - info.getValueType(lang).length() - 1; i++)
+                    out.print(" ");
+                out.print(" ");
+                out.print(info.getIdentifier());
+                for(int i=0; i<lens[1] - info.getIdentifier().length(); i++)
+                    out.print(" ");
+
+                out.print(" = ");
+                out.print(info.getValueCode(lang));
+                out.print(";");
+
+                text = info.getComment(lang);
+                if(text != null && !text.isEmpty()) {
+                    for(int i=0; i<lens[2] - info.value.length(); i++)
+                        out.print(" ");
+
+                    out.print(" //");
+                    out.print(info.getComment(lang));
+                }
+                out.println();
+            }
+
+
+            out.println();
+        }
+    }
+
+    private static void outputJava(String lang, String outdir) throws IOException {
         String path = outdir + "\\" + output_package.replace('.', '\\');
         new File(path).mkdirs();
 
@@ -159,42 +251,14 @@ public class GenConstants {
         out.println();
         out.println("public class " + output_name + " {");
 
-        for(String key : group_order_list) {
-            List<ConstantInfo> list = output_constants.get(key);
-            int[] lens = getKeyValueLength(list);
-
-            for(ConstantInfo info : list) {
-                out.print("    public static final ");
-                out.print(info.getValueType("Java"));
-                out.print(" ");
-                out.print(info.getIdentifier());
-
-                for(int i=0; i<lens[0] - info.getIdentifier().length(); i++)
-                    out.print(" ");
-
-                out.print(" = ");
-                out.print(info.value);
-                out.print(";");
-                if(!info.comment.isEmpty()) {
-                    for(int i=0; i<lens[1] - info.value.length(); i++)
-                        out.print(" ");
-
-                    out.print(" //");
-                    out.print(info.comment);
-                }
-                out.println();
-            }
-
-
-            out.println();
-        }
+        outputItems(lang, out);
 
         out.println("}");
 
         out.close();
     }
 
-    private static void outputCpp(String outdir) throws IOException {
+    private static void outputCpp(String lang, String outdir) throws IOException {
         new File(outdir).mkdirs();
 
         File file = new File(outdir + "\\" + output_name.toLowerCase() + ".h");
@@ -206,34 +270,7 @@ public class GenConstants {
         out.println("{");
         out.println("public:");
 
-        for(String key : group_order_list) {
-            List<ConstantInfo> list = output_constants.get(key);
-            int[] lens = getKeyValueLength(list);
-
-            for(ConstantInfo info : list) {
-                out.print("    static const ");
-                out.print(info.getValueType("C++"));
-                out.print(" ");
-                out.print(info.getIdentifier());
-
-                for(int i=0; i<lens[0] - info.getIdentifier().length(); i++)
-                    out.print(" ");
-
-                out.print(" = ");
-                out.print(info.value);
-                out.print(";");
-                if(!info.comment.isEmpty()) {
-                    for(int i=0; i<lens[1] - info.value.length(); i++)
-                        out.print(" ");
-
-                    out.print(" //");
-                    out.print(info.comment);
-                }
-                out.println();
-            }
-
-            out.println();
-        }
+        outputItems(lang, out);
 
         out.println("};");
 
